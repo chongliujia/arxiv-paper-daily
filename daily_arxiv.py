4fef159d3a1af1d9d20e28382a66b10e176a7244
chongliujia <jcl314159@gmail.com>
2022-02-14 02:52:51 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -4,6 +4,10 @@ import json
 import arxiv
 import os
 
+
+base_url = "https://arxiv.paperswithcode.com/api/v0/papers/"
+
+
 def get_authors(authors, first_author = False):
     output = str()
     if first_author == False:
@@ -24,6 +28,9 @@ def sort_papers(papers):
 
 def get_daily_papers(topic, query="object detection", max_results=3):
     content = dict()
+    content_to_web = dict()
+
+    cnt = 0
 
     search_engine = arxiv.Search(
             query       = query,
@@ -35,6 +42,8 @@ def get_daily_papers(topic, query="object detection", max_results=3):
         paper_id    = result.get_short_id()
         paper_title = result.title
         paper_url   = result.entry_id
+
+        code_url    = base_url + paper_id
 
         paper_abstract = result.summary.replace("\n", " ")
         paper_authors  = get_authors(result.authors)
@@ -55,11 +64,35 @@ def get_daily_papers(topic, query="object detection", max_results=3):
         else:
             paper_key = paper_id[0:ver_pos]
 
-        content[paper_key] = f"|**{publish_time}**|**{paper_title}**|{paper_first_author} et.al.|[{paper_id}]({paper_url})|\n"
+        #content[paper_key] = f"|**{publish_time}**|**{paper_title}**|{paper_first_author} et.al.|[{paper_id}]({paper_url})|\n"
+        try:
+            r = requests.get(code_url).json()
+            # source code link
+            if "official" in r and r["official"]:
+                cnt += 1
+                repo_url = r["official"]["url"]
+                content[paper_key] = f"|**{update_time}**|**{paper_title}**|{paper_first_author} et.al.|[{paper_id}]({paper_url})|**[link]({repo_url})**|\n"
+                content_to_web[paper_key] = f"- {update_time}, **{paper_title}**, {paper_first_author} et.al., Paper: [{paper_url}]({paper_url}), Code: **[{repo_url}]({repo_url})**"
+
+            else:
+                content[paper_key] = f"|**{update_time}**|**{paper_title}**|{paper_first_author} et.al.|[{paper_id}]({paper_url})|null|\n"
+                content_to_web[paper_key] = f"- {update_time}, **{paper_title}**, {paper_first_author} et.al., Paper: [{paper_url}]({paper_url})"
+
+            # TODO: select useful comments
+            comments = None
+            if comments != None:
+                content_to_web[paper_key] = content_to_web[paper_key] + f", {comments}\n"
+            else:
+                content_to_web[paper_key] = content_to_web[paper_key] + f"\n"
+
+        except Exception as e:
+            print(f"exception: {e} with id: {paper_key}")
+
 
     data = {topic:content}
+    data_web = {topic:content_to_web}
 
-    return data
+    return data, data_web
 
 
 def update_json_file(filename, data_all):
@@ -104,23 +137,35 @@ def json_to_md(filename):
         pass
 
     with open(md_filename, "a+") as f:
-        f.write("## Updated on " + DateNow + "\n\n")
+        if (use_title == True) and (to_web == True):
+            f.write("---\n" + "layout: default\n" + "---\n\n")
 
+        if use_title == True:
+            f.write("## Updated on " + DateNow + "\n\n")
+        else:
+            f.write("> Updated on " + DateNow + "\n\n")
+        
         for keyword in data.keys():
             day_content = data[keyword]
             if not day_content:
                 continue
-
+            # the head of each part
             f.write(f"## {keyword}\n\n")
-            f.write("|Publish Date|Title|Authors|PDF|\n" + "|---|---|---|---|\n")
 
+            if use_title == True :
+                if to_web == False:
+                    f.write("|Publish Date|Title|Authors|PDF|Code|\n" + "|---|---|---|---|---|\n")
+                else:
+                    f.write("| Publish Date | Title | Authors | PDF | Code |\n")
+                    f.write("|:---------|:-----------------------|:---------|:------|:------|\n")
+
+            # sort papers by date
             day_content = sort_papers(day_content)
-
-            for _, v in day_content.items():
+        
+            for _,v in day_content.items():
                 if v is not None:
                     f.write(v)
 
-            f.write(f"\n")
 
     print("finished")
 
@@ -139,6 +184,7 @@ if __name__ == "__main__":
         print("Keyword: " + topic)
         data = get_daily_papers(topic, query = keyword, max_results = 6)
         data_collector.append(data)
+        data_collector_web.append(data_web)
         print("\n")
 
 
